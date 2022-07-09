a2d39bc4952a2c8b3e76e3fe4720b0a2d8fb68fd
Zheng Song <41896553+szhsin@users.noreply.github.com>
2022-07-09 11:59:42 +1000

    Create useId hook

--- dist/es/utils/constants.js
+++ dist/es/utils/constants.js
@@ -1,8 +1,9 @@
 import { createContext } from 'react';
 
 var ACCORDION_BLOCK = 'szh-accordion';
+var ACCORDION_PREFIX = 'szh-adn';
 var ACCORDION_BTN_ATTR = "data-" + ACCORDION_BLOCK + "-btn";
 var ACCORDION_ATTR = "data-" + ACCORDION_BLOCK;
 var AccordionContext = /*#__PURE__*/createContext({});
 
-export { ACCORDION_ATTR, ACCORDION_BLOCK, ACCORDION_BTN_ATTR, AccordionContext };
+export { ACCORDION_ATTR, ACCORDION_BLOCK, ACCORDION_BTN_ATTR, ACCORDION_PREFIX, AccordionContext };
