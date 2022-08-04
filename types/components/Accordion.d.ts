911b442f1e7c0f3ff6b19e34cefa7da241ee89e9
Zheng Song <41896553+szhsin@users.noreply.github.com>
2022-08-04 22:51:07 +1000

    Export typing for component props

--- types/components/Accordion.d.ts
+++ types/components/Accordion.d.ts
@@ -4,4 +4,4 @@ import { ControlledAccordionProps } from './ControlledAccordion';
 interface AccordionProps extends AccordionProviderOptions, Omit<ControlledAccordionProps, 'providerValue'> {
 }
 declare const Accordion: import("react").ForwardRefExoticComponent<AccordionProps & import("react").RefAttributes<HTMLDivElement>>;
-export { Accordion };
+export { Accordion, AccordionProps };
--- types/index.d.ts
+++ types/index.d.ts
@@ -2,4 +2,6 @@ export { Accordion } from './components/Accordion';
 export { ControlledAccordion } from './components/ControlledAccordion';
 export { AccordionItem } from './components/AccordionItem';
 export { useAccordionProvider } from './hooks/useAccordionProvider';
+export type { AccordionProps } from './components/Accordion';
+export type { ControlledAccordionProps } from './components/ControlledAccordion';
 export type { AccordionItemProps } from './components/AccordionItem';
