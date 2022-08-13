21fa829700f58e68ecfc03b915b3069629c93268
Zheng Song <41896553+szhsin@users.noreply.github.com>
2022-08-13 17:16:43 +1000

    Headless UI example code

---
+++ website/src/components/HeadlessUI/Accordion.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  useAccordion,
+  useAccordionProvider,
+  AccordionProvider
+} from '@szhsin/react-accordion';
+import styles from './styles.module.css';
+
+const Accordion = ({ children }: { children: React.ReactNode }) => {
+  const providerValue = useAccordionProvider({
+    // Omit these two options if you don't want to implement any transition
+    // highlight-start
+    transition: true,
+    transitionTimeout: 200
+    // highlight-end
+  });
+  const { accordionProps } = useAccordion();
+
+  return (
+    <AccordionProvider value={providerValue}>
+      <div className={styles.accordion} {...accordionProps}>
+        {children}
+      </div>
+    </AccordionProvider>
+  );
+};
+
+export default Accordion;
---
+++ website/src/components/HeadlessUI/AccordionItem.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {
+  useAccordionItem,
+  // highlight-next-line
+  useHeightTransition
+} from '@szhsin/react-accordion';
+import ChevronDown from '@site/static/img/chevron-down.svg';
+import styles from './styles.module.css';
+
+const AccordionItem = ({
+  header,
+  children,
+  itemKey,
+  initialEntered
+}: {
+  header: React.ReactNode;
+  children: React.ReactNode;
+  itemKey?: string | number;
+  initialEntered?: boolean;
+}) => {
+  const { itemRef, state, buttonProps, panelProps } =
+    useAccordionItem<HTMLDivElement>({ itemKey, initialEntered });
+
+  // highlight-start
+  const [transitionStyle, panelRef] =
+    useHeightTransition<HTMLDivElement>(state);
+  // highlight-end
+
+  const { status, isMounted, isEnter } = state;
+
+  return (
+    <div className={styles.item} ref={itemRef}>
+      <h3 style={{ margin: 0 }}>
+        <button
+          className={isEnter ? styles.buttonExpanded : styles.button}
+          type="button"
+          {...buttonProps}
+        >
+          {header}
+          <ChevronDown className={styles.chevron} />
+        </button>
+      </h3>
+      {isMounted && (
+        // Add an extra `div` around the panel `div` for the
+        // height transition to work as intended
+        // highlight-next-line
+        <div
+          className={styles.content}
+          style={{
+            display: status === 'exited' ? 'none' : undefined,
+            // highlight-next-line
+            ...transitionStyle
+          }}
+        >
+          <div
+            className={styles.panel}
+            // highlight-next-line
+            ref={panelRef}
+            {...panelProps}
+          >
+            {children}
+          </div>
+          {/* Closing tag of the extra `div` */}
+          {/* highlight-next-line */}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AccordionItem;
