cd066586ffc261fd31fe4ec23473f57769843f60
Zheng Song <szhsin9@gmail.com>
2022-08-18 21:03:14 +1000

    Add `array.map` example and update docs (#94)

---
+++ website/src/components/Starter/ArrayMap.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Accordion, AccordionItem } from '../accordion';
+
+const items = [
+  {
+    header: 'What is Lorem Ipsum?',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing...'
+  },
+  {
+    header: 'Where does it come from?',
+    content: 'Quisque eget luctus mi, vehicula mollis lorem...'
+  },
+  {
+    header: 'Why do we use it?',
+    content: 'Suspendisse massa risus, pretium id interdum in...'
+  }
+];
+
+export default function Example() {
+  return (
+    <Accordion>
+      {items.map(({ header, content }, i) => (
+        <AccordionItem header={header} key={i}>
+          {content}
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
+}
