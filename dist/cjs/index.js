cdc4e9775d73e17e14108bf869628b7ea8b46088
Zheng Song <41896553+szhsin@users.noreply.github.com>
2022-10-22 19:14:11 +1100

    Address Rollup v3 breaking changes

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-Object.defineProperty(exports, '__esModule', { value: true });
-
 var react = require('react');
 var reactTransitionState = require('react-transition-state');
 var jsxRuntime = require('react/jsx-runtime');
