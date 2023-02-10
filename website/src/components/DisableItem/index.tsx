e7ef728f23cb589fc614469afb1225d19f943e9c
Zheng Song <41896553+szhsin@users.noreply.github.com>
2023-02-10 23:35:38 +1100

    Add docs for disabling items

---
+++ website/src/components/DisableItem/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Accordion, AccordionItem } from '../accordion';
+
+export default function Example() {
+  return (
+    <Accordion>
+      <AccordionItem header="What is Lorem Ipsum?">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </AccordionItem>
+
+      <AccordionItem header="Where does it come from?" disabled>
+        Quisque eget luctus mi, vehicula mollis lorem. Proin fringilla
+        vel erat quis sodales. Nam ex enim, eleifend venenatis lectus
+        vitae, accumsan auctor mi.
+      </AccordionItem>
+
+      <AccordionItem header="Why do we use it?">
+        Suspendisse massa risus, pretium id interdum in, dictum sit amet
+        ante. Fusce vulputate purus sed tempus feugiat.
+      </AccordionItem>
+    </Accordion>
+  );
+}
