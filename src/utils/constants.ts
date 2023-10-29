65462fe867bbad039a8bc60be7d9a1ed108caa58
Zheng Song <41896553+szhsin@users.noreply.github.com>
2023-10-29 18:29:25 +1100

    chore: resolve break changes for major dep bumps

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,7 +8,7 @@ import {
 
 export const ACCORDION_BLOCK = 'szh-accordion';
 export const ACCORDION_PREFIX = 'szh-adn';
-export const ACCORDION_ATTR = `data-${ACCORDION_PREFIX}`;
+export const ACCORDION_ATTR: string = `data-${ACCORDION_PREFIX}`;
 export const ACCORDION_BTN_ATTR = `data-${ACCORDION_PREFIX}-btn`;
 
 export type Modifiers = {
