4c7331a0d823c75dca306535d9a33df4fd1b97b6
Zheng Song <szhsin9@gmail.com>
2025-07-05 20:38:31 +1000

    Update dependencies and fix breaking changes

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,4 +1,5 @@
-import clsx from 'clsx';
+import { type JSX } from 'react';
+import { clsx } from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
